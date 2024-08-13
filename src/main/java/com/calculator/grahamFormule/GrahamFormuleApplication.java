af976545e62fdbfff71d2680ef7f3c7af2f1d2a1
Eder Rodrigues <ederpr@gmail.com>
2024-08-13 20:46:22 -0300

    Commit inicial do projeto

---
+++ src/main/java/com/calculator/grahamFormule/GrahamFormuleApplication.java
@@ -0,0 +1,13 @@
+package com.calculator.grahamFormule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrahamFormuleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrahamFormuleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/calculator/grahamFormule/GrahamFormuleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.calculator.grahamFormule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrahamFormuleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

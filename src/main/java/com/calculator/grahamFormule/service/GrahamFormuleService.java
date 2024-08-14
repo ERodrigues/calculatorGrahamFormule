dcad92a46e5c07eaadae23894350b0ef93494c64
Eder Rodrigues <ederpr@gmail.com>
2024-08-14 20:55:10 -0300

    classe de servico para realizar as chamadas da entidade

--- src/main/java/com/calculator/grahamFormule/service/GrahamFormuleService.java
+++ src/main/java/com/calculator/grahamFormule/service/GrahamFormuleService.java
@@ -1,2 +1,11 @@
-package com.calculator.grahamFormule.service;public class GrahamFormuleService {
+package com.calculator.grahamFormule.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class GrahamFormuleService {
+
+    public double calculate(double vpaValue, double lpaValue) {
+        return 0.0;
+    }
 }

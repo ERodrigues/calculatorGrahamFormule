4f6c4c7f018cc45c349a939f2ecbc62102898692
Eder Rodrigues <ederpr@gmail.com>
2024-08-13 22:02:51 -0300

    Implementacao do calculo da formula de graham

--- src/main/java/com/calculator/grahamFormule/entity/GrahamFormule.java
+++ src/main/java/com/calculator/grahamFormule/entity/GrahamFormule.java
@@ -1,11 +1,26 @@
 package com.calculator.grahamFormule.entity;
 
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
+import java.util.Locale;
+
 public class GrahamFormule {
 
+    private static final double CONSTANT_GRAHAM = 22.5;
+    private Double vpa;
+    private Double lpa;
+
     public GrahamFormule(double vpa, double lpa) {
+        this.vpa = vpa;
+        this.lpa = lpa;
     }
 
     public Double calculate() {
-        return 0.0;
+        return formatResult(Math.sqrt(CONSTANT_GRAHAM * lpa * vpa));
+    }
+
+    private Double formatResult(double value){
+        DecimalFormat formatValue = new DecimalFormat("#.##", new DecimalFormatSymbols(Locale.ENGLISH));
+        return Double.valueOf(formatValue.format(value));
     }
 }

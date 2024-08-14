ccfbe83a34c3b853f8612fb74261c4e434833c30
Eder Rodrigues <ederpr@gmail.com>
2024-08-14 09:06:35 -0300

    add metodo de validacao

--- src/main/java/com/calculator/grahamFormule/entity/GrahamFormule.java
+++ src/main/java/com/calculator/grahamFormule/entity/GrahamFormule.java
@@ -7,12 +7,21 @@ import java.util.Locale;
 public class GrahamFormule {
 
     private static final double CONSTANT_GRAHAM = 22.5;
-    private Double vpa;
-    private Double lpa;
+    private final Double vpa;
+    private final Double lpa;
 
     public GrahamFormule(double vpa, double lpa) {
         this.vpa = vpa;
         this.lpa = lpa;
+        validate();
+    }
+
+    private void validate() {
+        if (vpa < 0.0)
+            throw new IllegalArgumentException("Not support negative vpa attribute");
+
+        if (lpa < 0.0)
+            throw new IllegalArgumentException("Not support negative lpa attribute");
     }
 
     public Double calculate() {

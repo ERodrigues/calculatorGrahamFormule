e6a067b434283e61ef1216966de38b0a6fa36772
Eder Rodrigues <ederpr@gmail.com>
2024-08-13 22:02:23 -0300

    Implementacao dos testes para calculo da formula de graham

--- src/main/java/com/calculator/grahamFormule/entity/GrahamFormule.java
+++ src/main/java/com/calculator/grahamFormule/entity/GrahamFormule.java
@@ -1,2 +1,11 @@
-package com.calculator.grahamFormule.entity;public class GrahamFormule {
+package com.calculator.grahamFormule.entity;
+
+public class GrahamFormule {
+
+    public GrahamFormule(double vpa, double lpa) {
+    }
+
+    public Double calculate() {
+        return 0.0;
+    }
 }
--- src/test/java/com/calculator/grahamFormule/entity/GrahamFormuleTest.java
+++ src/test/java/com/calculator/grahamFormule/entity/GrahamFormuleTest.java
@@ -19,4 +19,28 @@ class GrahamFormuleTest {
         grahamFormule = new GrahamFormule(0.0, 0.0);
         assertEquals(grahamFormule.calculate(), 0.0);
     }
+
+    @Test
+    public void should_return_zero_when_lpa_is_one_and_vpa_is_zero(){
+        grahamFormule = new GrahamFormule(0.0, 1.0);
+        assertEquals(grahamFormule.calculate(), 0.0);
+    }
+
+    @Test
+    public void should_return_zero_when_lpa_is_zero_and_vpa_is_one(){
+        grahamFormule = new GrahamFormule(1.0, 0.0);
+        assertEquals(grahamFormule.calculate(), 0.0);
+    }
+
+    @Test
+    public void should_return_4_74_when_vpa_is_one_and_lpa_is_one(){
+        grahamFormule = new GrahamFormule(1.0, 1.0);
+        assertEquals(grahamFormule.calculate(), 4.74);
+    }
+
+    @Test
+    public void should_return_23_72_when_vpa_is_five_and_lpa_is_five(){
+        grahamFormule = new GrahamFormule(5.0, 5.0);
+        assertEquals(grahamFormule.calculate(), 23.72);
+    }
 }

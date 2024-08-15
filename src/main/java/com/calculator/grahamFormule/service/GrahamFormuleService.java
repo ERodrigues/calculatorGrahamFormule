489a1a070839dca1e31eacecf64ce1b3ae7817e5
Eder Rodrigues <ederpr@gmail.com>
2024-08-14 21:00:12 -0300

    Chamada do calculo da formula de graham

--- src/main/java/com/calculator/grahamFormule/service/GrahamFormuleService.java
+++ src/main/java/com/calculator/grahamFormule/service/GrahamFormuleService.java
@@ -1,11 +1,12 @@
 package com.calculator.grahamFormule.service;
 
+import com.calculator.grahamFormule.entity.GrahamFormule;
 import org.springframework.stereotype.Service;
 
 @Service
 public class GrahamFormuleService {
 
     public double calculate(double vpaValue, double lpaValue) {
-        return 0.0;
+        return new GrahamFormule(vpaValue, lpaValue).calculate();
     }
 }
